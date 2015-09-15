8c565f29074a05f1293a30c4061d449e57c1e2dc
styoung30 <styoung306@gmail.com>
2015-09-15 11:00:13 -0400

    changes with processor timers

--- ShawnYoung_homework2.R
+++ ShawnYoung_homework2.R
@@ -4,11 +4,14 @@ d6 <- function(){
 roll2 <- function(){
   sum(d6() + d6())
 }
+rollbegin <-proc.time()
 rolls <-replicate(10^6, roll2())
-
+rollend <- proc.time()
+print(rollend - roll begin)
 #Set starting positions
 squareHistory <-NULL
 currentSquare <- 1
+mainbegin <- proc.time()
 for(i in 1:length(rolls)){
   currentSquare = (currentSquare + rolls[i]) %% 40
   if(identical(currentSquare, 31)){
@@ -19,6 +22,9 @@ for(i in 1:length(rolls)){
     squareHistory[i] = currentSquare
   }
 }
+mainend <- proc.time()
+print(mainend - mainbegin)
+
 odds <- NULL
 oddstable <- table(squareHistory)
 print(oddstable)
